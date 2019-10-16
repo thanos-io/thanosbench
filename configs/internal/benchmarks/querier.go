ee401b3f3c3699b5708d8f502104c9f2d3436bc1
Bartek Plotka <bwplotka@gmail.com>
2019-10-16 20:20:25 +0100

    Added Store GW and Querier.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

---
+++ configs/internal/benchmarks/querier.go
@@ -0,0 +1,184 @@
+package bench
+
+import (
+	"fmt"
+
+	"github.com/bwplotka/mimic"
+	"github.com/bwplotka/mimic/encoding"
+	"github.com/go-openapi/swag"
+	"github.com/thanos-io/thanosbench/configs/abstractions/dockerimage"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+type QuerierOpts struct {
+	Namespace string
+	Name      string
+
+	Img       dockerimage.Image
+	Resources corev1.ResourceRequirements
+
+	StoreAPILabelSelector string
+
+	// TODO(bwplotka): Add static storeAPIs option.
+}
+
+func GenThanosQuerier(gen *mimic.Generator, opts QuerierOpts) {
+	const (
+		replicas = 1
+		httpPort = 19190
+		grpcPort = 19090
+	)
+
+	// Special headless k8s service for SRV lookup to discover local StoreAPIs.
+	// https://kubernetes.io/docs/concepts/services-networking/service/#headless-services
+	storeAPISrv := corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      opts.Name + "-store-apis",
+			Namespace: opts.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeClusterIP,
+			Selector: map[string]string{
+				opts.StoreAPILabelSelector: "true",
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "grpc",
+					Port:       grpcPort,
+					TargetPort: intstr.FromInt(grpcPort),
+				},
+			},
+			ClusterIP: corev1.ClusterIPNone,
+		},
+	}
+
+	srv := corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      opts.Name,
+			Namespace: opts.Namespace,
+			Labels: map[string]string{
+				selectorName: opts.Name,
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeClusterIP,
+			Selector: map[string]string{
+				selectorName: opts.Name,
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Port:       httpPort,
+					TargetPort: intstr.FromInt(httpPort),
+				},
+				{
+					Name:       "grpc",
+					Port:       grpcPort,
+					TargetPort: intstr.FromInt(grpcPort),
+				},
+			},
+		},
+	}
+
+	querierContainer := corev1.Container{
+		Name:    "thanos",
+		Image:   opts.Img.String(),
+		Command: []string{"thanos"},
+		Args: []string{
+			"query",
+			"--log.level=debug",
+			"--debug.name=$(POD_NAME)",
+			// Large limits!
+			fmt.Sprintf("--query.max-concurrent=%d", 99999999),
+			fmt.Sprintf("--query.timeout=%s", "2h"),
+			fmt.Sprintf("--query.replica-label=%s", "replica"),
+			fmt.Sprintf("--http-address=0.0.0.0:%d", httpPort),
+			fmt.Sprintf("--grpc-address=0.0.0.0:%d", grpcPort),
+
+			fmt.Sprintf("--store=dnssrv+%s.default.svc.cluster.local:%d", storeAPISrv.Name, grpcPort),
+		},
+		Env: []corev1.EnvVar{
+			// NOTE: Add following env var for old go memory management: {Name: "GODEBUG", Value:"madvdontneed=1"}.
+			{Name: "POD_NAME", ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "metadata.name",
+				},
+			}},
+		},
+		ImagePullPolicy: corev1.PullAlways,
+		ReadinessProbe: &corev1.Probe{
+			Handler: corev1.Handler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path: "-/ready",
+					Port: intstr.FromInt(httpPort),
+				},
+			},
+			SuccessThreshold: 3,
+		},
+		LivenessProbe: &corev1.Probe{
+			Handler: corev1.Handler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path: "/-/healthy",
+					Port: intstr.FromInt(httpPort),
+				},
+			},
+			InitialDelaySeconds: 30,
+			TimeoutSeconds:      30,
+		},
+		Ports: []corev1.ContainerPort{
+			{
+				Name:          "http",
+				ContainerPort: httpPort,
+			},
+			{
+				Name:          "grpc",
+				ContainerPort: grpcPort,
+			},
+		},
+		Resources: opts.Resources,
+	}
+
+	dpl := appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      opts.Name,
+			Namespace: opts.Namespace,
+			Labels: map[string]string{
+				selectorName: opts.Name,
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: swag.Int32(replicas),
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						selectorName: opts.Name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{querierContainer},
+				},
+			},
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					selectorName: opts.Name,
+				},
+			},
+		},
+	}
+	gen.Add(opts.Name+".yaml", encoding.GhodssYAML(storeAPISrv, dpl, srv))
+}
--- configs/main.go
+++ configs/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"github.com/bwplotka/mimic"
 	"github.com/thanos-io/thanosbench/configs/abstractions/dockerimage"
+	"github.com/thanos-io/thanosbench/configs/abstractions/secret"
 	bench "github.com/thanos-io/thanosbench/configs/internal/benchmarks"
 	"gopkg.in/alecthomas/kingpin.v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 const (
@@ -31,5 +34,61 @@ func main() {
 
 		bench.GenRemoteReadBenchPrometheus(generator, "prometheus", namespace, dockerimage.PublicPrometheus("v2.12.0"), dockerimage.PublicThanos("v0.7.0"))
 		bench.GenRemoteReadBenchPrometheus(generator, "prometheus-rr-streamed", namespace, dockerimage.PublicPrometheus("v2.13.0"), dockerimage.PublicThanos("v0.7.0"))
+	}
+	{
+		generator := generator.With("store-gateway", "gen-manifests")
+
+		const storeAPILabelSelector = "store-test-api"
+		bench.GenThanosStoreGateway(generator, bench.StoreGatewayOpts{
+			Name:      "store",
+			Namespace: namespace,
+			Img:       dockerimage.PublicThanos("v0.8.1"),
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("1"),
+					corev1.ResourceMemory: resource.MustParse("8Gi"),
+				},
+				Limits: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("1"),
+					corev1.ResourceMemory: resource.MustParse("8Gi"),
+				},
+			},
+			IndexCacheBytes: "250MB",
+			ChunkCacheBytes: "2GB",
+
+			StoreAPILabelSelector: storeAPILabelSelector,
+
+			// You need secret for this.
+			/*
+				apiVersion: v1
+				kind: Secret
+				metadata:
+				  name: s3
+				data:
+				  s3.yaml: |
+				    <base64 config>
+			*/
+			ObjStoreSecret: secret.NewFile(
+				"s3.yaml",
+				"s3",
+				"/s3/config",
+			),
+		})
+		bench.GenThanosQuerier(generator, bench.QuerierOpts{
+			Name:      "query",
+			Namespace: namespace,
+			Img:       dockerimage.PublicThanos("v0.8.1"),
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("1"),
+					corev1.ResourceMemory: resource.MustParse("2Gi"),
+				},
+				Limits: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("1"),
+					corev1.ResourceMemory: resource.MustParse("2Gi"),
+				},
+			},
+			StoreAPILabelSelector: storeAPILabelSelector,
+		})
 	}
 }
