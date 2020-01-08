1534bcf1d414bb926419839fb1ac4533d3dc25a5
Giedrius Statkevičius <giedriuswork@gmail.com>
2020-01-08 23:48:30 +0200

    thanosbench: stress: add initial implementation
    Signed-off-by: Giedrius Statkevičius <giedriuswork@gmail.com>

--- cmd/thanosbench/stress.go
+++ cmd/thanosbench/stress.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+
+	"time"
+
 	"github.com/go-kit/kit/log"
 	"github.com/oklog/run"
+	"github.com/pkg/errors"
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/thanos-io/thanos/pkg/store/storepb"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -10,15 +23,84 @@ func registerStress(m map[string]setupFunc, app *kingpin.Application) {
 	cmd := app.Command("stress", "Stress tests a remote StoreAPI.")
 	workers := cmd.Flag("workers.num", "Number of go routines for stress testing.").Required().Int()
 	target := cmd.Arg("target", "IP:PORT pair of the target to stress.").IP()
+	timeout := cmd.Arg("timeout", "TTimeout in seconds of each operation").Default("60").Int()
+	lookback := cmd.Arg("query.look-back", "How much time into the past at max we should look back").Default("7776000").Int()
 
 	// TODO(GiedriusS): send other requests like Info() as well.
+	// TODO(GiedriusS): we could ask for random aggregations.
 	m["stress"] = func(g *run.Group, logger log.Logger) error {
 		g.Add(func() error {
-			var _ = workers
-			var _ = target
-			return nil
-		}, func(error) {
+			conn, err := grpc.Dial(target.String(), grpc.WithInsecure())
+			if err != nil {
+				return err
+			}
+			defer conn.Close()
+			c := storepb.NewStoreClient(conn)
 
+			lblvlsCtx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout)*time.Nanosecond)
+			defer cancel()
+
+			labelvaluesResp, err := c.LabelValues(lblvlsCtx, &storepb.LabelValuesRequest{Label: labels.MetricName})
+			if err != nil {
+				return err
+			}
+			if len(labelvaluesResp.Warnings) > 0 {
+				return errors.New(fmt.Sprintf("got %#v warnings from LabelValues() call", labelvaluesResp.Warnings))
+			}
+			labelvalues := labelvaluesResp.Values
+			if len(labelvalues) == 0 {
+				return errors.New("the StoreAPI responded with zero metric names")
+			}
+
+			g, ctx := errgroup.WithContext(context.Background())
+
+			for i := 0; i < *workers; i++ {
+				g.Go(func() error {
+					opCtx, cancel := context.WithTimeout(ctx, time.Duration(*timeout)*time.Nanosecond)
+					defer cancel()
+
+					randomMetric := labelvalues[rand.Intn(len(labelvalues))]
+					max := time.Now().Unix()
+					min := time.Now().Unix() - rand.Int63n(int64(*lookback))
+
+					r, err := c.Series(opCtx, &storepb.SeriesRequest{
+						MinTime: min * 1000,
+						MaxTime: max * 1000,
+						Matchers: []storepb.LabelMatcher{
+							storepb.LabelMatcher{
+								Type:  storepb.LabelMatcher_EQ,
+								Name:  labels.MetricName,
+								Value: randomMetric,
+							},
+						},
+						Aggregates: []storepb.Aggr{storepb.Aggr_RAW, storepb.Aggr_COUNTER},
+					}, grpc.MaxCallRecvMsgSize(math.MaxInt32))
+
+					if err != nil {
+						return err
+					}
+
+					for {
+						seriesResp, err := r.Recv()
+						if err == io.EOF {
+							break
+						}
+						if err != nil && err != io.EOF {
+							return err
+						}
+						series := seriesResp.GetSeries()
+						if series == nil {
+							continue
+						}
+					}
+
+					return nil
+				})
+			}
+
+			return g.Wait()
+		}, func(err error) {
+			level.Info(logger).Log("msg", "stress test encountered an error", "err", err.String())
 		})
 		return nil
 	}
--- go.mod
+++ go.mod
@@ -1,22 +1,32 @@
 module github.com/thanos-io/thanosbench
 
 require (
+	github.com/a8m/mark v0.1.1-0.20170507133748-44f2db618845 // indirect
 	github.com/bwplotka/mimic v0.0.0-20190730202618-06ab9976e8ef
+	github.com/gernest/wow v0.1.0 // indirect
 	github.com/go-kit/kit v0.9.0
+	github.com/go-openapi/analysis v0.19.4 // indirect
+	github.com/go-openapi/runtime v0.19.3 // indirect
 	github.com/go-openapi/swag v0.19.4
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.1-0.20191002090509-6af20e3a5340 // indirect
+	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
+	github.com/minio/cli v1.20.0 // indirect
 	github.com/oklog/run v1.0.0
 	github.com/oklog/ulid v1.3.1
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/common v0.7.0
-	github.com/prometheus/prometheus v1.8.2-0.20190913102521-8ab628b35467 // v1.8.2 is misleading as Prometheus does not have v2 module.
-	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/thanos-io/thanos v0.8.1-0.20191015125029-5d107d1c3cdc
+	github.com/prometheus/prometheus v1.8.2-0.20191126064551-80ba03c67da1 // v1.8.2 is misleading as Prometheus does not have v2 module.
+	github.com/thanos-io/thanos v0.9.0
+	github.com/uber-go/atomic v1.4.0 // indirect
+	go.mongodb.org/mongo-driver v1.0.4 // indirect
 	go.uber.org/automaxprocs v1.2.0
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	google.golang.org/grpc v1.25.1
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
-	gopkg.in/yaml.v2 v2.2.2
-	k8s.io/api v0.0.0-20190813020757-36bff7324fb7
-	k8s.io/apimachinery v0.0.0-20190809020650-423f5d784010
+	gopkg.in/yaml.v2 v2.2.5
+	k8s.io/api v0.0.0-20191115095533-47f6de673b26
+	k8s.io/apimachinery v0.0.0-20191115015347-3c7067801da2
+	k8s.io/client-go v12.0.0+incompatible // indirect
 )
 
 // We want to replace the client-go version with a specific commit hash,
